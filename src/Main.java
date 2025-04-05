4b37fa2c5ca97735dcd2f5b91e6d9a965f64269c
s33492 <s33492@pjwstk.edu.pl>
2025-04-05 14:14:17 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MusicLibrary myLibrary = new MusicLibrary();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        myLibrary.addTrack("Led Zeppelin - Stairway to Heaven");
+        myLibrary.addTrack("Queen - Bohemian Rhapsody");
+        myLibrary.addTrack("Pink Floyd - Comfortably Numb");
+        myLibrary.addTrack("AC/DC - Back in Black");
+        myLibrary.addTrack("Metallica - Nothing Else Matters");
+        myLibrary.addTrack("Led Zeppelin - Stairway to Heaven");
+        myLibrary.addTrack("Queen -  Rhapsody");
+
+
+
+        System.out.println();
+
+        myLibrary.displayTracks();
+        System.out.println();
+
+
+        myLibrary.createPlaylist("Favorites");
+        myLibrary.createPlaylist("Party");
+        myLibrary.createPlaylist("Favorites");
+
+        System.out.println();
+
+
+        System.out.println();
+
+        myLibrary.addTrackToPlaylist("Queen - Bohemian Rhapsody",
+                "Favorites");
+        myLibrary.addTrackToPlaylist("Pink Floyd - Comfortably Numb",
+                "Favorites");
+        myLibrary.addTrackToPlaylist("AC/DC - Back in Black", "Party");
+        myLibrary.addTrackToPlaylist("Metallica - Nothing Else Matters",
+                "Party");
+        System.out.println();
+
+
+        myLibrary.displayPlaylist("Party");
+
+        myLibrary.searchTracks("Queen");
+        System.out.println();
+        myLibrary.searchTracks("Numb");
+        System.out.println();
+
+
+        myLibrary.removeTrack("AC/DC - Back in Black");
+        myLibrary.displayPlaylist("Party");
+        myLibrary.displayTracks();
+        System.out.println();
+
+
+        myLibrary.displayAllPlaylists();
+
+        System.out.println("Total number of tracks in the library: " +
+                myLibrary.getTrackCount());
+        System.out.println("Total number of playlists: " +
+                myLibrary.getPlaylistCount());
     }
+
 }
